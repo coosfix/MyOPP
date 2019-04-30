c5850df8f0c3884c17ed8b1e88f858a6081809d3
stepmania003 <stepmania003@gmail.com>
2019-04-30 11:11:52 +0800

    作業

---
+++ MyOOP/MyOOP/ClsBanking.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOOP
+{
+    class ClsBanking
+    {
+        public decimal _balance;
+        //將資料分裝在類別
+        public decimal balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                if (value > 0) _balance = value;
+            }
+        }
+        private int _P1;
+        public int P1
+        {
+            get { return _P1; }
+            set { _P1 = value; }
+        }
+
+
+        public int P2 { get { return 100; } }
+        public int P3 { get; set; }
+
+        private int[] num = { 5, 12, 4, 6 };
+        //自訂索引子
+        public int this[int i]
+        {
+            get
+            {
+                if (i > num.Length - 1 || i < 0)
+                {
+                    throw new IndexOutOfRangeException("範圍不正確");
+                }
+                return num[i];
+            }
+        }
+        //多載
+        public int this[string s]
+        {
+            get
+            {
+                int i = int.Parse(s);
+                if (i > num.Length - 1 || i < 0)
+                {
+                    throw new IndexOutOfRangeException("範圍不正確");
+                }
+                return num[i];
+            }
+        }
+
+
+
+
+        //可以給予不同存取規則
+        public int P4 { get; private set; }
+        //自動實做屬性
+        public int P5 { get; set; }
+        public int P6;
+        
+        public void Deposit(decimal money)
+        {
+            this._balance += money;
+        }
+
+        public decimal Withdraw(decimal money)
+        {
+            this._balance -= money;
+            return this._balance;
+        }
+    }
+}
---
+++ MyOOP/MyOOP/FrmOOP.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyOOP
+{
+    public partial class FrmOOP : Form
+    {
+        public FrmOOP()
+        {
+            InitializeComponent();
+        }
+        ClsBanking user1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            user1 = new ClsBanking();
+            MessageBox.Show(user1.balance.ToString("C2"));
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // GEt
+            this.label1.Text = user1.balance.ToString("C2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Set
+            user1.balance = decimal.Parse(this.textBox1.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //可寫可讀
+            user1.P1 = 1000;
+            MessageBox.Show(user1.P1.ToString());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //唯讀
+            //user1.P2 = 100;
+            MessageBox.Show(user1.P2.ToString());
+        }
+        ClsBanking sss = new ClsBanking();
+        ClsBanking vvv = new ClsBanking();
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //唯寫
+            // user1.P3 = 100;
+            //int w = user1.P3;
+            //sss.aaa = 100;
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            List<Point> list = new List<Point>();
+            list.Add(new Point(100, 200));
+            list.Add(new Point(100, 800));
+            list.Add(new Point(100, 100));
+            list.Add(new Point(100, 400));
+            this.dataGridView1.DataSource = list;
+            //不用資料繫節
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    this.listBox2.Items.Add(list[i].X+","+ list[i].Y);
+            //}
+
+            this.listBox2.DataSource = list;
+            //屬性資料繫節
+            List<ClsBanking> Mylist = new List<ClsBanking>();
+            Mylist.Add(new ClsBanking { balance = 999, P1 = 100, P3 = 200 });
+            Mylist.Add(new ClsBanking { balance = 888 });
+            this.dataGridView2.DataSource = Mylist;
+
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //
+            this.categoriesTableAdapter1.Fill(this.nwDataSet1.Categories);
+            this.productsTableAdapter1.Fill(this.nwDataSet1.Products);
+            this.customersTableAdapter1.Fill(this.nwDataSet1.Customers);
+            this.dataGridView1.DataSource = nwDataSet1.Products;
+            this.dataGridView2.DataSource = nwDataSet1.Categories;
+            this.dataGridView3.DataSource = nwDataSet1.Customers;
+
+            MessageBox.Show(nwDataSet1.Products.Rows[0][1].ToString());
+            //針對Products的Row中的ProductName屬性取值
+            MessageBox.Show(nwDataSet1.Products[0].ProductName);
+            
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ClsBanking user1 = new ClsBanking();
+            MessageBox.Show(user1[0].ToString());
+            try
+            {
+                MessageBox.Show(user1[10].ToString());
+            }
+            catch (IndexOutOfRangeException EX)
+            {
+
+                MessageBox.Show(EX.Message);
+            }
+            catch(Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //可以給予不同存取規則
+            ClsBanking user1 = new ClsBanking();
+            MessageBox.Show(user1.P4.ToString());
+            //無法set user1.P4=99;
+        }
+
+            ClsBanking user2 = new ClsBanking();
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+            user2.Deposit(1000);
+            label4.Text = user2._balance.ToString();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            user2.Withdraw(1000);
+            label4.Text = user2._balance.ToString();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            ClsBanking rr = new ClsBanking();
+            //屬性多載 索引子
+            MessageBox.Show(rr[1].ToString());
+            MessageBox.Show(rr["1"].ToString());
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            //方法多載
+            //public void TEst(int....)
+            //public void TEst(string,string...)
+          
+        }
+
+        
+    }
+}
